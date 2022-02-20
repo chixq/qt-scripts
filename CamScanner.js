7a437e56533bed7a40fb213d57c6a29e64919ec1
Kris Chi <chilaoqi@gmail.com>
2022-02-21 00:04:13 +0800

    Update CamScanner.js

--- CamScanner.js
+++ CamScanner.js
@@ -14,5 +14,5 @@ QX & Q & Surge Mitm = ap*.intsig.net,
 */
 
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"1675267359"}}};
 $done({body: JSON.stringify(obj)});
